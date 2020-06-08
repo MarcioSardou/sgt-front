f552e0dfe676fc9ef363b460bb6f7de8aea4cc79
Marcio Sardou <marciosut1996@gmail.com>
2020-06-08 20:07:49 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import Routes from './routes'
 import GlobalStyle from './styles/global'
+import api from './services/api'
 
 function App() {
+  useEffect(() => {
+    api
+      .post('/api', {
+        body: `query{
+        allTeachers{
+          edges{
+            node {id, nome }
+          }
+          totalCount
+        }
+      }`,
+      })
+      .then((response) => console.log('> ', response))
+  }, [])
+
   return (
     <BrowserRouter>
       <Routes />
